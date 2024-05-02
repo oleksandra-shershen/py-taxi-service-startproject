59ca1078657e7dc389ffb04a5e8ad22467f9c7d5
Shershen Alexandra <hornet240204@gmail.com>
2024-05-02 23:04:21 +0300

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -6,14 +6,9 @@ from django.db import models
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(
-        "Manufacturer",
-        on_delete=models.CASCADE,
-        related_name="cars"
+        "Manufacturer", on_delete=models.CASCADE, related_name="cars"
     )
-    drivers = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        related_name="cars"
-    )
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
 
     class Meta:
         ordering = ("model",)
@@ -24,10 +19,7 @@ class Car(models.Model):
 
 class Driver(AbstractUser):
     license_number = models.CharField(
-        max_length=255,
-        unique=True,
-        null=True,
-        blank=True
+        max_length=255, unique=True, null=True, blank=True
     )
 
     class Meta:
